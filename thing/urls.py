ee9862b9737aab834aa4739d2609de323d59eec6
Nazar <nazar.hrytsiv7@gmail.com>
2019-03-11 17:06:27 +0300

    #issues remake static and templates route,add create to things via pure ajax and jQuery

--- thing/urls.py
+++ thing/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='thing-home'),
+    path('', views.show_things, name='thing-home'),
+    path('create/', views.create_thing, name='thing-create')
 ]
