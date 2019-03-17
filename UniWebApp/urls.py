8cbfa819604c5c9b27642fe2b35a8a745b269e9c
Nazar <nazar.hrytsiv7@gmail.com>
2019-03-17 12:58:58 +0300

    make a register form and logic

--- UniWebApp/urls.py
+++ UniWebApp/urls.py
@@ -20,7 +20,7 @@ from django.contrib import admin
 from django.urls import path, include
 from products.views import ProductListView,ProductDetailView,create_product, edit_product, make_sale_all, ProductDetailSlugView, ProductSaleListView,ProductSaleDetailView
 from thing.views import create, show_things
-from .views import login_page
+from .views import login_page, register_page
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -29,6 +29,7 @@ urlpatterns = [
     path('user/', include('user.urls')),
     path('', include('thing.urls')),
     path('login/', login_page),
+    path('register/', register_page),
     path('products/', include("products.urls")),
     path('product/create/', create_product),
     # path('sale_all/', make_sale_all, name='make-sale-all'),
--- UniWebApp/views.py
+++ UniWebApp/views.py
@@ -1,7 +1,9 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
-from .forms import LoginForm
-from django.contrib.auth import authenticate, login
+from .forms import LoginForm, RegisterForm
+from django.contrib.auth import authenticate, login,get_user_model
 
+User = get_user_model()
 
 def login_page(request):
     form = LoginForm(request.POST or None)
@@ -17,6 +19,19 @@ def login_page(request):
             login(request,user)
             return redirect('/')
         else:
-            print("Error")
+            HttpResponse(status=403)
 
     return render(request, 'login.html', context)
+
+def register_page(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form':form
+    }
+    if form.is_valid():
+        username = form.cleaned_data.get("username")
+        email = form.cleaned_data.get("email")
+        password = form.cleaned_data.get("password")
+        new_user = User.objects.create_user(username,email,password)
+        print(new_user)
+    return render(request, 'registration.html', context)
